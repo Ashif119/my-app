d34f814326968ca1a434babf8b40c7964b6cacdf
Rounpay <76778687+Rounpay@users.noreply.github.com>
2026-02-11 18:58:38 +0530

    Button Change

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css'
+import Button from './components/Button'
 
 function App() {
   return (
     <>
     <h1 className='mb-4'>Ashif Kadri</h1>
     <p className='text-green-700'>Android & Flutter Developer</p>
+    <Button/>
     </>
   )
 }
---
+++ src/components/Button.tsx
@@ -0,0 +1,6 @@
+
+
+const Button = ({text = "Submit"})=>{
+    return <button className="bg-blue-500">{text}</button>
+}
+export default Button
